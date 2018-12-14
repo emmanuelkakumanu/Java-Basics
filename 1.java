ea9505210e9638334537e8cd7eba0e7518fdca43
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-14 10:19:36 +0530

    class,method,object

---
+++ 1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Ideone obj=new Ideone();
+		obj.fun1();
+	}
+	public void fun1()
+	{
+		System.out.print("Function Called!");
+	}
+}
