7a79861db272fe8eda192b7b52b91e462f401bdd
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-14 10:40:07 +0530

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Ideone obj=new Ideone();
+		obj.Print_Name();
+	}
+	void Print_Name()
+	{
+		System.out.println("My name is Emmanuel.");
+	}
+}
