f74ba2297395ef75967da5e9a7c9a95b72441037
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-14 10:35:30 +0530

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=7;
+		boolean b=false;
+		char ch='^';
+		float f=4.25f;
+		double d=5.9987d;
+		System.out.println("Int:"+a);
+		System.out.println("Boolean:"+b);
+		System.out.println("char:"+ch);
+		System.out.println("float:"+f);
+		System.out.println("double:"+d);
+	}
+}
