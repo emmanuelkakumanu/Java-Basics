88c6597beac6279ef5d091c85689544c8fa580f9
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-14 10:32:12 +0530

    Create 3.java

---
+++ 3.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here (Single line comment)
+		
+		/* first
+		day at 
+		jala tech
+		(Multi line comments)
+		*/
+		
+		
+	}
+}
